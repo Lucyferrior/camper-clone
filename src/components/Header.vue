263d6e86788f23075e1008706db87d6dbbb87c7d
aleyna-soyer <aleynasoyer9@gmail.com>
2022-11-16 13:01:31 +0300

    Header componenti oluşturuldu

---
+++ src/components/Header.vue
@@ -0,0 +1,60 @@
+<template>
+    <header class="style_headerNav__1B5VI">
+        <div class="headroom-wrapper" style="height: 42px;">
+            <div style="position:relative;top:0;left:0;right:0;z-index:1;-webkit-transform:translate3D(0, 0, 0);-ms-transform:translate3D(0, 0, 0);transform:translate3D(0, 0, 0)"
+                class="headroom headroom--unfixed">
+                <div class="sc-bUQyIj exIfBg tnm tnm--desktop"><a href="https://www.camper.com/tr_TR"
+                        class="sc-iXeHaJ xCnor"><img alt="Camper"
+                            src="https://www.camper.com/cms/api/assets/tiers-banners-homes/03158e4d-c9ae-4945-8a35-cb1362d17021/camper-logo.svg"
+                            width="64px" height="33px"></a>
+                    <div class="sc-ecQkzk vPeNx"><span class="tnm__title "><a
+                                href="https://www.camper.com/tr_TR/women/shoes"
+                                class="sc-gtsrHT gCZJJF sc-eCApnc iRwvKm trackable ">Kadın</a>
+
+
+                        </span></div>
+                    <div class="sc-ecQkzk vPeNx"><span class="tnm__title "><a
+                                href="https://www.camper.com/tr_TR/men/shoes" title="Camper Erkek"
+                                class="sc-gtsrHT gCZJJF sc-eCApnc iRwvKm trackable ">Erkek</a>
+                            <div class="sc-jlQhrW iHvgiu tnm__highlights tnm__highlights--4">
+                            
+                                
+                                <div class="sc-iXquSf bwRbQ"></div>
+                            </div>
+                        </span></div>
+                    <div class="sc-ecQkzk vPeNx"><span class="tnm__title "><a
+                                href="https://www.camper.com/tr_TR/kids/shoes"
+                                class="sc-gtsrHT gCZJJF sc-eCApnc iRwvKm trackable ">Çocuk</a>
+                           
+                        </span></div>
+                    <div class="sc-ecQkzk vPeNx"><span class="tnm__title "><a
+                                href="https://www.camper.com/tr_TR/landings/fall_winter_collection_lab"
+                                title="CAMPERLAB" class="sc-gtsrHT gCZJJF sc-eCApnc iRwvKm trackable ">CAMPERLAB</a>
+                            
+                        </span></div>
+                    <div class="sc-ecQkzk vPeNx"><span class="tnm__title "><a
+                                href="https://www.camper.com/tr_TR/content/stories" title="Camper Stories"
+                                class="sc-gtsrHT gCZJJF sc-eCApnc iRwvKm trackable ">Stories</a></span></div>
+                    <div class="sc-tsGVs liNYvh"><a href="https://www.camper.com/tr_TR/search"
+                            class="sc-dSnXvR dzBcKB last-item"><span class="searcherspan"><input type="text"
+                                    placeholder="ARAMA" class="searcher"><span class="searchericon"><img alt="Search"
+                                        src="https://www.camper.com/cms/api/assets/tiers-banners-homes/33b5eba4-3c7e-4547-805c-8a94b80e73e6/search.svg"
+                                        width="17px" height="17px"></span></span>
+
+                        </a><a href="https://www.camper.com/tr_TR/shops" class="sc-JsfZP kBcPwt">Mağaza bul</a>
+                        <div class="sc-hYRTwp iRaesw">
+                            <div class="style_accountIcon__3Mfxg"><span class="style_label__3Pufn">Hesabım</span></div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "header-vue"
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderVue>
+      
+    </HeaderVue>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import HeaderVue from '@/components/Header.vue';
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HeaderVue,
+   
   }
 }
 </script>

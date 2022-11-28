ddd6a0eb97debae7d32a24c97ad61c7e8bd057f5
aleyna-soyer <aleynasoyer9@gmail.com>
2022-11-28 23:52:12 +0300

    Footer.vue oluşturuldu

---
+++ src/components/Footer.vue
@@ -0,0 +1,201 @@
+<template>
+<footer class="sc-eEVmNe illmTA">
+    <div
+        style="margin-left:-15px;margin-right:-15px;display:flex;flex-wrap:wrap;flex-grow:0;flex-shrink:0;align-items:normal;justify-content:flex-start;flex-direction:row">
+        <div style="box-sizing:border-box;min-height:1px;position:relative;padding-left:15px;padding-right:15px;width:29.166666666666668%;flex-basis:29.166666666666668%;flex-grow:0;flex-shrink:0;max-width:29.166666666666668%;margin-left:0%;right:auto;left:auto"
+            class="sc-fmdNqN hyACbC">
+            <div class="sc-uxdHp fiDOnB">
+                <div class="sc-eHEENL gAtBhL">
+                    <h2 class="sc-eCApnc wTYNf">Cyber Monday için Ek %10 İndirim</h2>
+                    <div class="sc-hzUIXc jSTRnN">
+                        <p class="sc-eCApnc irDGJG">Camper ailesinin bir parçası olmak size özel tekliflere, indirimlere
+                            ve işbirliklerine erişim sağlar.</p>
+                    </div>
+                    <div class="sc-kYPZxB kZgSIK"><a cursor="pointer" class="sc-jSFjdj bNRfQR sc-xGAEC cBoHoo">Abone
+                            Ol</a></div>
+                </div>
+            </div>
+        </div>
+        <div style="box-sizing:border-box;min-height:1px;position:relative;padding-left:15px;padding-right:15px;width:29.166666666666668%;flex-basis:29.166666666666668%;flex-grow:0;flex-shrink:0;max-width:29.166666666666668%;margin-left:0%;right:auto;left:41.66666666666667%"
+            class="sc-fmdNqN hyACbC">
+            <div class="sc-ljsmAU jFDlvU">
+                <div class="sc-eGJWMs gQYBcx sc-jlZJtj QgNfY">
+                    <div class="sc-csTbgd kglWtV"><img alt="phone"
+                            src="https://www.camper.com/cms/api/assets/tiers-banners-homes/f081eabe-64d6-4e08-b700-686b4630966b/phone.svg"
+                            width="30px" height="30px"></div>
+                    <div>
+                        <p class="sc-eCApnc fmYzey">Müşteri Hizmetleri</p>
+                        <p class="sc-eCApnc irDGJG">Ayrıca, iletişim formunu doldurarak da bizimle iletişime
+                            geçebilirsiniz. Çalışma saatlerimiz her gün 09:00 - 19:00. Resmi tatiller hariç.</p><a
+                            class="sc-gtsrHT dIMSXt sc-eCApnc irDGJG trackable"
+                            data-tracking="eventGA|customer-service|call|0850 777 27 37" href="tel:0850 777 27 37">Bizi
+                            arayın: 0850 777 27 37</a>
+                    </div>
+                    <div></div>
+                </div>
+                <div class="sc-eGJWMs gQYBcx sc-jlZJtj QgNfY">
+                    <div class="sc-csTbgd kglWtV"><img alt="store-search"
+                            src="https://www.camper.com/cms/api/assets/tiers-banners-homes/ef860a11-e37f-408b-a10d-c663785f833b/store-search.svg"
+                            width="30px" height="34px"></div>
+                    <div>
+                        <p class="sc-eCApnc fmYzey">Camper mağazalari</p>
+                        <p class="sc-eCApnc irDGJG"></p><a href="https://www.camper.com/tr_TR/shops"
+                            class="sc-gtsrHT dIMSXt sc-eCApnc irDGJG">Size en yakin mağazayi bulun</a>
+                    </div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+        <div style="box-sizing:border-box;min-height:1px;position:relative;padding-left:15px;padding-right:15px;width:41.66666666666667%;flex-basis:41.66666666666667%;flex-grow:0;flex-shrink:0;max-width:41.66666666666667%;margin-left:0%;right:29.166666666666668%;left:auto"
+            class="sc-fmdNqN hyACbC">
+            <div class="sc-dWBRfb iWqacH">
+                <div class="sc-jHcXXw lkkECO">
+                    <div
+                        style="margin-left:-15px;margin-right:-15px;display:flex;flex-wrap:wrap;flex-grow:0;flex-shrink:0;align-items:normal;justify-content:flex-start;flex-direction:row">
+                        <div style="box-sizing:border-box;min-height:1px;position:relative;padding-left:15px;padding-right:15px;width:33.333333333333336%;flex-basis:33.333333333333336%;flex-grow:0;flex-shrink:0;max-width:33.333333333333336%;margin-left:0%;right:auto;left:auto"
+                            class="sc-jXcxbT eINlL">
+                            <div>
+                                <p class="sc-eCApnc hMrhVu">Camper.com alişverişleriniz</p>
+                            </div><input type="checkbox" class="sc-bQCEYZ irmCui"><img alt="Open"
+                                src="https://www.camper.com/cms/api/assets/tiers-banners-homes/674a7db4-e669-4c49-8c92-90de3353f1f6/chevron.svg">
+                            <ul class="sc-dlnjwi sc-fXgAZx dJXsSm erAkNO">
+                                <li class="sc-hKFxyN sc-EZqKI kksiKu iONckA">
+                                    <p cursor="pointer" class="sc-eCApnc wgBem">Sipariş takibi</p>
+                                </li>
+                                <li class="sc-hKFxyN sc-EZqKI kksiKu iONckA"><a
+                                        href="https://www.camper.com/tr_TR/at_cliente_faq?faq=21"
+                                        class="sc-gtsrHT gCZJJF sc-eCApnc irDGJG">Ödeme</a></li>
+                                <li class="sc-hKFxyN sc-EZqKI kksiKu iONckA"><a
+                                        href="https://www.camper.com/tr_TR/at_cliente_faq?question=46"
+                                        class="sc-gtsrHT gCZJJF sc-eCApnc irDGJG">Gönderim</a></li>
+                                <li class="sc-hKFxyN sc-EZqKI kksiKu iONckA"><a
+                                        href="https://www.camper.com/tr_TR/at_cliente_faq?tit=5"
+                                        class="sc-gtsrHT gCZJJF sc-eCApnc irDGJG">Iade ve değişimler</a></li>
+                                <li class="sc-hKFxyN sc-EZqKI kksiKu iONckA"><a
+                                        href="https://www.camper.com/tr_TR/at_cliente_faq?faq=51"
+                                        class="sc-gtsrHT gCZJJF sc-eCApnc irDGJG">Ürün garantisi</a></li>
+                                <li class="sc-hKFxyN sc-EZqKI kksiKu iONckA"><a
+                                        href="https://www.camper.com/tr_TR/sitemap"
+                                        class="sc-gtsrHT gCZJJF sc-eCApnc irDGJG">Sitemap</a></li>
+                            </ul>
+                        </div>
+                        <div style="box-sizing:border-box;min-height:1px;position:relative;padding-left:15px;padding-right:15px;width:33.333333333333336%;flex-basis:33.333333333333336%;flex-grow:0;flex-shrink:0;max-width:33.333333333333336%;margin-left:0%;right:auto;left:auto"
+                            class="sc-jXcxbT eINlL">
+                            <div>
+                                <p class="sc-eCApnc hMrhVu">Müşteri hizmetleri</p>
+                            </div><input type="checkbox" class="sc-bQCEYZ irmCui"><img alt="Open"
+                                src="https://www.camper.com/cms/api/assets/tiers-banners-homes/674a7db4-e669-4c49-8c92-90de3353f1f6/chevron.svg">
+                            <ul class="sc-dlnjwi sc-fXgAZx dJXsSm erAkNO">
+                                <li class="sc-hKFxyN sc-EZqKI kksiKu iONckA"><a
+                                        href="https://www.camper.com/tr_TR/customer_service"
+                                        class="sc-gtsrHT gCZJJF sc-eCApnc irDGJG">Sss</a></li>
+                                <li class="sc-hKFxyN sc-EZqKI kksiKu iONckA"><a
+                                        href="https://www.camper.com/tr_TR/contacto"
+                                        class="sc-gtsrHT gCZJJF sc-eCApnc irDGJG">Iletişim</a></li>
+                                <li class="sc-hKFxyN sc-EZqKI kksiKu iONckA"><a
+                                        href="https://www.camper.com/html/legal/pdfs/privacy/privacy_VF_EN.pdf"
+                                        target="_blank" class="sc-gtsrHT gCZJJF sc-eCApnc irDGJG">Gizlilik ilkeleri</a>
+                                </li>
+                                <li class="sc-hKFxyN sc-EZqKI kksiKu iONckA"><a
+                                        href="https://www.camper.com/html/legal/TC/TC_TR00_TR_TR.htm" target="_blank"
+                                        class="sc-gtsrHT gCZJJF sc-eCApnc irDGJG">Kullanim koşullari</a></li>
+                            </ul>
+                        </div>
+                        <div style="box-sizing:border-box;min-height:1px;position:relative;padding-left:15px;padding-right:15px;width:33.333333333333336%;flex-basis:33.333333333333336%;flex-grow:0;flex-shrink:0;max-width:33.333333333333336%;margin-left:0%;right:auto;left:auto"
+                            class="sc-jXcxbT eINlL">
+                            <div>
+                                <p class="sc-eCApnc hMrhVu">Camper hakkinda</p>
+                            </div><input type="checkbox" class="sc-bQCEYZ irmCui"><img alt="Open"
+                                src="https://www.camper.com/cms/api/assets/tiers-banners-homes/674a7db4-e669-4c49-8c92-90de3353f1f6/chevron.svg">
+                            <ul class="sc-dlnjwi sc-fXgAZx dJXsSm erAkNO">
+                                <li class="sc-hKFxyN sc-EZqKI kksiKu iONckA"><a
+                                        href="https://www.camper.com/tr_TR/content/history"
+                                        class="sc-gtsrHT gCZJJF sc-eCApnc irDGJG">Tarihçe</a></li>
+                                <li class="sc-hKFxyN sc-EZqKI kksiKu iONckA"><a
+                                        href="https://www.camper.com/tr_TR/together"
+                                        class="sc-gtsrHT gCZJJF sc-eCApnc irDGJG">Camper together</a></li>
+                                <li class="sc-hKFxyN sc-EZqKI kksiKu iONckA"><a
+                                        href="https://www.camper.com/tr_TR/content/social_responsibility"
+                                        class="sc-gtsrHT gCZJJF sc-eCApnc irDGJG">Social Responsibility</a></li>
+                                <li class="sc-hKFxyN sc-EZqKI kksiKu iONckA"><a
+                                        href="https://www.camper.com/tr_TR/contacto?type=8"
+                                        class="sc-gtsrHT gCZJJF sc-eCApnc irDGJG">Iş imkanlari</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="sc-gIvpjk dVRMhB">
+        <div style="box-sizing:border-box;min-height:1px;position:relative;padding-left:0;padding-right:0;width:100%;flex-basis:100%;flex-grow:0;flex-shrink:0;max-width:100%;margin-left:0%;right:auto;left:auto"
+            class="column ">
+            <div class="sc-fHCHyC crlswz b-corp"><a href="https://www.camper.com/int/content/bcorp"><img
+                        src="https://camper.com/cms/api/assets/tiers-banners-homes/07972d9c-e695-4986-b660-74be9ced5854/bcorp-v2.svg"
+                        alt="b-corp"></a></div>
+        </div>
+    </div>
+    <div class="sc-gIvpjk dVRMhB" style="border:0">
+        <div style="box-sizing:border-box;min-height:1px;position:relative;padding-left:0;padding-right:0;width:29.166666666666668%;flex-basis:29.166666666666668%;flex-grow:0;flex-shrink:0;max-width:29.166666666666668%;margin-left:0%;right:auto;left:auto"
+            class="column">
+            <div class="sc-fHCHyC crlswz">
+                <p class="sc-eCApnc irDGJG">© Camper, 2022</p>
+            </div>
+        </div>
+        <div style="box-sizing:border-box;min-height:1px;position:relative;padding-left:0;padding-right:0;width:29.166666666666668%;flex-basis:29.166666666666668%;flex-grow:0;flex-shrink:0;max-width:29.166666666666668%;margin-left:0%;right:auto;left:auto"
+            class="column">
+            <div class="sc-dtLLSn dpehyt">
+                <div class="sc-ikXwFM hZHGfK"><img
+                        src="https://www.camper.com/cms/api/assets/tiers-banners-homes/ed38f07d-6cd4-473d-be93-d3a713722a04/tr.svg"
+                        alt="Türkiye" loading="lazy" width="30px" height="23px"></div>
+                <p cursor="pointer" class="sc-eCApnc iqSVpL"> </p><u>
+                    <p cursor="pointer" class="sc-eCApnc iqSVpL">Türkiye
+                        <!-- --> /
+                        <!-- -->Türkçe
+                    </p>
+                </u>
+            </div>
+        </div>
+        <div style="box-sizing:border-box;min-height:1px;position:relative;padding-left:0;padding-right:0;width:41.66666666666667%;flex-basis:41.66666666666667%;flex-grow:0;flex-shrink:0;max-width:41.66666666666667%;margin-left:0%;right:auto;left:auto"
+            class="column">
+            <div class="sc-fHCHyC sc-dkQUaI crlswz hQrVZb">
+                <div class="sc-dFRpbK bFgzLZ sc-WZYut jLeSfx">
+                    <div class="sc-bsatvv gHEtUc"><a href="https://www.facebook.com/Camper" target="_blank"
+                            rel="noopener noreferrer"><img alt="facebook"
+                                src="https://www.camper.com/cms/api/assets/tiers-banners-homes/87497d33-586d-4a79-9fa5-c47354ab68ca/facebook.svg"
+                                width="24px" height="24px"></a></div>
+                </div>
+                <div class="sc-dFRpbK bFgzLZ sc-WZYut jLeSfx">
+                    <div class="sc-bsatvv gHEtUc"><a href="https://www.tiktok.com/@camper?lang=en " target="_blank"
+                            rel="noopener noreferrer"><img alt="tiktok"
+                                src="https://www.camper.com/cms/api/assets/tiers-banners-homes/3fb534a7-733c-48c0-b970-803bb4530013/tiktok.svg"
+                                width="24px" height="24px"></a></div>
+                </div>
+                <div class="sc-dFRpbK bFgzLZ sc-WZYut jLeSfx">
+                    <div class="sc-bsatvv gHEtUc"><a href="https://www.linkedin.com/company/camper" target="_blank"
+                            rel="noopener noreferrer"><img alt="linkedin"
+                                src="https://www.camper.com/cms/api/assets/tiers-banners-homes/dbf580d8-c41b-4fd9-974e-b95f2631a7df/linkedin.svg"
+                                width="24px" height="24px"></a></div>
+                </div>
+                <div class="sc-dFRpbK bFgzLZ sc-WZYut jLeSfx">
+                    <div class="sc-bsatvv gHEtUc"><a href="https://instagram.com/camper/" target="_blank"
+                            rel="noopener noreferrer"><img alt="instagram"
+                                src="https://www.camper.com/cms/api/assets/tiers-banners-homes/8009cc8f-2e97-4ba1-b4a8-a830b62b0481/instagram.svg"
+                                width="24px" height="24px"></a></div>
+                </div>
+                <div class="sc-dFRpbK bFgzLZ sc-WZYut jLeSfx">
+                    <div class="sc-bsatvv gHEtUc"><a href="https://www.pinterest.com/campershoes/" target="_blank"
+                            rel="noopener noreferrer"><img alt="pinterest"
+                                src="https://www.camper.com/cms/api/assets/tiers-banners-homes/36db0a8a-3344-4de0-a6d1-046e8280f518/pinterest.svg"
+                                width="24px" height="24px"></a></div>
+                </div>
+                <div class="sc-dFRpbK bFgzLZ sc-WZYut jLeSfx">
+                    <div class="sc-bsatvv gHEtUc"><a href="https://www.youtube.com/user/Camper" target="_blank"
+                            rel="noopener noreferrer"><img alt="youtube"
+                                src="https://www.camper.com/cms/api/assets/tiers-banners-homes/3f6f04fe-212f-4377-86ad-c8b6a27576f7/youtube.svg"
+                                width="24px" height="24px"></a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>

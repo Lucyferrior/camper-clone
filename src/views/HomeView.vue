faabcc45656d6cbbe21394cfa46e3cdcb31ef9ac
aleyna-soyer <aleynasoyer9@gmail.com>
2022-12-02 12:41:04 +0300

    Fotos: import edildi

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,23 @@
 <template>
   <div class="home">
-    <HeaderVue>
-      
-    </HeaderVue>
+    <HeaderVue></HeaderVue>
     <FooterVue></FooterVue>
+    <FotosVue></FotosVue>
   </div>
 </template>
 
 <script>
   import HeaderVue from '@/components/Header.vue';
   import FooterVue from '@/components/Footer.vue';
+  import FotosVue from '@/components/Fotos.vue';
 
 
 export default {
   name: 'HomeView',
   components: {
     HeaderVue,
-    FooterVue
+    FooterVue,
+    FotosVue
    
   }
 }

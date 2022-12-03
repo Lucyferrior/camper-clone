838ea040063505b71b93d16a5d29679cefe191f4
aleyna-soyer <aleynasoyer9@gmail.com>
2022-12-03 17:52:11 +0300

    dropdown:oluşturuldu, import edildi

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@
     <HeaderVue></HeaderVue>
     <FooterVue></FooterVue>
     <FotosVue></FotosVue>
+    <DropdownVue></DropdownVue>
   </div>
 </template>
 
@@ -10,15 +11,15 @@
   import HeaderVue from '@/components/Header.vue';
   import FooterVue from '@/components/Footer.vue';
   import FotosVue from '@/components/Fotos.vue';
-
+  import DropdownVue from '@/components/Dropdown.vue';
 
 export default {
   name: 'HomeView',
   components: {
     HeaderVue,
     FooterVue,
-    FotosVue
-   
+    FotosVue,
+    DropdownVue,
   }
 }
 </script>
